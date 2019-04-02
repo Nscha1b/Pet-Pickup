ac3bc72641701b35823b107b57e08ab33e81816d
Nick Schafer <Nps61992@gmail.com>
2019-04-01 21:29:43 -0400

    Seriously the table is working now
    Changed the table back to updating when someone modifies a case.

--- Pet-Pickup/src/app/dashboard/dashboard.component.ts
+++ Pet-Pickup/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { PetCaseService } from '../shared/pet-case.service';
-import { PetCase } from '../shared/pet-case.model';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatSort } from '@angular/material/';
 
@@ -13,42 +12,22 @@ import { MatSort } from '@angular/material/';
 })
 
 
-export class DashboardComponent implements AfterViewInit, OnInit {
+export class DashboardComponent implements AfterViewInit {
   columnsToDisplay = [
     'id', 'firstname', 'pet', 'sex', 'weight', 'crematory',
     'type', 'clinic', 'status'];
-  dataSource: MatTableDataSource<PetCase>;
-
+  dataSource = new MatTableDataSource(this.petCaseService.getCases());
    @ViewChild(MatPaginator) paginator: MatPaginator;
    @ViewChild(MatSort) sort: MatSort;
 
   constructor(private petCaseService: PetCaseService) {}
-
   clinics = new FormGroup({
     'clinicDD': new FormControl('All Clinics')
   });
 
   ngAfterViewInit() {
-    // Mat data tables will not let you sort on nested object properties,
-    // so for whatever item we are referring to on the table,
-    // return the nested property
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      switch (property) {
-        case 'id': return item.person.id
-        case 'firstname': return item.person.firstname;
-        case 'pet': return item.pet.petname;
-        case 'sex': return item.pet.sex;
-        case 'weight': return item.pet.petweight;
-        case 'crematory': return item.cremationDetails.crematory;
-        case 'type': return item.cremationDetails.type;
-        case 'clinic': return item.cremationDetails.clinic;
-        case 'status': return item.cremationDetails.status;
-        default:
-          return item[property];
-      }
-    };
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.getNestedProps();
+    this.refresh();
   }
 
   filterVetClinic(clinicDD) {
@@ -60,11 +39,36 @@ export class DashboardComponent implements AfterViewInit, OnInit {
     this.dataSource.filter = clinicDD;
   }
 
-  ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.petCaseService.getCases());
+  refresh() {
+    this.petCaseService.casesChanged.subscribe(() => {
+      this.dataSource = new MatTableDataSource(this.petCaseService.getCases());
+      this.getNestedProps();
+    });
   }
 
-}
+  // Mat data tables will not let you sort on nested object properties,
+  // so for whatever item we are referring to on the table,
+  // return the nested property
+  getNestedProps() {
+      this.dataSource.sortingDataAccessor = (item, property) => {
+        switch (property) {
+          case 'id': return item.person.id
+          case 'firstname': return item.person.firstname;
+          case 'pet': return item.pet.petname;
+          case 'sex': return item.pet.sex;
+          case 'weight': return item.pet.petweight;
+          case 'crematory': return item.cremationDetails.crematory;
+          case 'type': return item.cremationDetails.type;
+          case 'clinic': return item.cremationDetails.clinic;
+          case 'status': return item.cremationDetails.status;
+          default:
+            return item[property];
+        }
+      };
+      this.dataSource.sort = this.sort;
+    }
+  }
+
 
 
 
