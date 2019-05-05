feef56549b02f9bcb34a03ba58e8df14f1a26182
Nick Schafer <Nps61992@gmail.com>
2019-05-05 12:41:38 -0400

    changed bcrypt to bcryptjs
    changed bcrypt to bcryptjs

--- Pet-Pickup/backend/routes/api.js
+++ Pet-Pickup/backend/routes/api.js
@@ -1,6 +1,6 @@
 const Router = require("express-promise-router");
 const db = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const checkAuth = require("../middleware/check-auth");
 
